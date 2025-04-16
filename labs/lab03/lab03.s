a61b85aa230fb126ab82768ed00136d290276eef
Leonardo Rodrigues <leonardo.rodrigues7@yahoo.com.br>
2025-04-16 14:41:17 -0300

    Finish Lab 03

--- labs/lab03/lab03.s
+++ labs/lab03/lab03.s
@@ -3,7 +3,7 @@
 
 bin_to_dec:
 
-    #li  t0, 0b1101      # Exemplo de número binário para teste
+    #li  t0, 0b1101      # Exemplo de número binário para testes
     lw  t0, 0(a0)        # (Parâmetro de entrada)
     li  t1, 0x01         # máscara para pegar o bit menos significativo
     li  t4, 0            # resultado final
