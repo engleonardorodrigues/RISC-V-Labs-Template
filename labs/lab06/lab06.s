748a64a9e7e2b423944f67abb8f4c1419ff2912c
Leonardo <leonardo.rodrigues7@yahoo.com.br>
2025-04-15 22:17:20 -0300

    Lab 6 initial tests

--- labs/lab06/lab06.s
+++ labs/lab06/lab06.s
@@ -2,10 +2,30 @@
 
 .global stack_func
 
+.data
+    stack_data:     .word 1, 2, 3, 4    # elementos da pilha
+    stack_op:       .word 1             # 1 = Push, 0 = Pop
+#    result:         .space 4            # para guardar o resultado de t1
+
+stack: 
+
+#    .text
+#    .globl main
+
+main:
+
+#    la, a0, stack_data  # usado para debugar o asm sem necessidade do main.c
+#    la, a1, stack_op    # usado para debugar o asm sem necessidade do main.c
+#    la, a2, result      # usado para debugar o asm sem necessidade do main.c
+
 stack_func:
+    
+    li t0, 0        # contador do sistema
+    mv t1, a0       # carrega todos os elementos da pilha
+    lw t2, 0(a1)    # carrega função da pilha (Push ou Pop)
+    addi a2, a0, 0  
 
-    lw t0, 0(a0)    # carrega elemento da pilha
-
-    add t1, t1, 
+finish:
+    sw t1, 0(a2)       
 
     ret
--- labs/lab06/main.c
+++ labs/lab06/main.c
@@ -3,7 +3,7 @@
 #define POP  0
 #define PUSH 1
 
-extern void stack_func(int* a, int* op, int* result);
+extern void stack_func(int* arg, int* op, int* result);
 
 int main(){
     
@@ -11,17 +11,20 @@ int main(){
     int result = 0;
     int arg = 0;
 
+    stack_func(arg, PUSH, &result);
+    printf("Push the %d, in the stack\n", arg);
+/*
     // Push in stack
     for (arg = 0; arg <= 5; arg++){
             stack_func(arg, PUSH, &result);
-            print("Push the %d, in the stack\n", arg);
+            printf("Push the %d, in the stack\n", arg);
     }
 
     // Pop on stack
     for (arg = 0; arg <= 5; arg++){
             stack_func(arg, POP, &result);
-            print("Pop the %d, on the stack\n", arg);
+            printf("Pop the %d, on the stack\n", arg);
     }
-
+*/
     return 0;
 }
