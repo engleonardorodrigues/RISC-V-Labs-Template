f448044c72566752df0445ca3a51147bb65ef825
Leonardo Rodrigues <leonardo.rodrigues7@yahoo.com.br>
2025-04-16 09:41:34 -0300

    Lab 06

--- labs/lab06/lab06.s
+++ labs/lab06/lab06.s
@@ -1,6 +1,6 @@
-.section .text
+#.section .text
 
-.global stack_func
+#.global stack_func
 
 .data
     stack_data:     .word 1, 2, 3, 4    # elementos da pilha
@@ -9,23 +9,23 @@
 
 stack: 
 
-#    .text
-#    .globl main
+    .text
+    .globl main
 
 main:
 
-#    la, a0, stack_data  # usado para debugar o asm sem necessidade do main.c
-#    la, a1, stack_op    # usado para debugar o asm sem necessidade do main.c
-#    la, a2, result      # usado para debugar o asm sem necessidade do main.c
+    la, a0, stack_data  # usado para debugar o asm sem necessidade do main.c
+    la, a1, stack_op    # usado para debugar o asm sem necessidade do main.c
+    la, a2, result      # usado para debugar o asm sem necessidade do main.c
 
 stack_func:
     
     li t0, 0        # contador do sistema
     mv t1, a0       # carrega todos os elementos da pilha
     lw t2, 0(a1)    # carrega função da pilha (Push ou Pop)
-    addi a2, a0, 0  
+    addi a2, a2, 5    
 
 finish:
-    sw t1, 0(a2)       
+    sw t3, 0(a2)      
 
     ret
