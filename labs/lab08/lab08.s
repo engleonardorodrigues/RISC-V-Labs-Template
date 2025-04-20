23848c18f11e39287dc302b90a1e8b796827f274
Leonardo <leonardo.rodrigues7@yahoo.com.br>
2025-04-20 17:36:04 -0300

    Finish Lab 08

--- labs/lab08/lab08.s
+++ labs/lab08/lab08.s
@@ -7,18 +7,28 @@
 
 fibonacci:
 
-    li t0, 0        # contador
-    lw t1, 0(a0)    # carrega N
+    li   t0, 0                # contador
+#   li   t1, 6                # carrega N localmente para teste
+    lw   t1, 0(a0)            # carrega N
+    li   t2, 0                # valor inicial de t2
+    li   t3, 1                # valor inicial de t3
+    li   t4, 0                # variavel para comparação 
 
-loop:
-    beqz  t0, t1, finish    # Se T0 e T1 forem iguais a sequencia fibonacci está concluida
-    
-for_loop:
+    sw   t2, 0(a1)            # salva primeiro valor da sequência fibonacci
+    addi a1, a1, 4            # avança para a próxima posição da memória (4 bytes)
 
-    add
-    addi  t0, t0, 1         # atualiza contador
+fibonacci_seq:
+
+    beq  t0, t1, finish       # Se T0 e T1 forem iguais a sequencia fibonacci está concluida
+    mv   t2, t3               # move o valor de t3 para t2
+    mv   t3, t4               # move o valor de t4 para t3
+    add  t4, t2, t3           # soma t2 + t3 e guarda em t4
+
+    sw   t4, 0(a1)            # salva resultado de t4 em a1
+    addi a1, a1, 4            # avança para a próxima posição da memória (4 bytes) 
+    addi t0, t0, 1            # avança o contador em uma unidade
+    j    fibonacci_seq        # retornar para loop for
 
 finish:    
 
-    sw t2, 0(a1)     # salva o valor de T2 no argumento a1
 ret
--- labs/lab08/main.c
+++ labs/lab08/main.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 
-extern void func(int* N, int* Fibonacci_sequence);
+extern void fibonacci(int* N, int* Fibonacci_sequence);
 
 int main(){
 
-    int N = 6, Fibonacci_sequence;
+    int N = 6;
+    int Fibonacci_sequence[N];
 
-    fibonacci(&N, &Fibonacci_sequence);
-    printf("The Fibonacci Sequence for N = %d is: %d \n", N, Fibonacci_sequence);
+    fibonacci(&N, Fibonacci_sequence);
+
+        printf("The Fibonacci Sequence for N = %d is: ", N);
+
+        for(int i = 0; i < N; i++)
+        {
+            printf("%d ", Fibonacci_sequence[i]);
+        }
+
+        printf("\n");
 
     return 0;
 }
